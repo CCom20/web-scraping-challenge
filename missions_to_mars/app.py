39d4b61df92abebab09ba274b5c57c14d65d5302
Cole Comstock <cole.d.comstock@gmail.com>
2020-12-01 19:41:27 -0700

    Finished up the app, just need to make the webpage pretty.

--- missions_to_mars/app.py
+++ missions_to_mars/app.py
@@ -1,14 +1,31 @@
-# import necessary libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from selenium import webdriver
+import scrape_mars
+import pymongo
 
-# create instance of Flask app
+# Create an instance of our Flask app.
 app = Flask(__name__)
 
+# Create connection variable
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
+mongo = PyMongo(app)
 
-# create route that renders index.html template
-@app.route("/")
-def echo():
-    return render_template("index.html", text="Serving up cool text from the Flask server!!")
+@app.route('/')
+def index():
+    mars_db = mongo.db.mars_db.find_one()
+    print(mars_db)
+
+    # Return the template with the teams list passed in
+    return render_template('index.html', mars=mars_db)
+
+# Set route
+@app.route('/scrape')
+def scraper():
+    mars_db = mongo.db.mars_db
+    mars_data = scrape_mars.scrape()
+    mars_db.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- missions_to_mars/scrape_mars.py
+++ missions_to_mars/scrape_mars.py
@@ -1,11 +1,27 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import pymongo
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Set up Mongo Connection / DB
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# Recreate DB without Duplicates
+mars_db = client.mars_db
+
+# Drops collection if available to remove duplicates
+mars_db.items.drop()
+
+# Create Collection again
+collection = mars_db.items
+
+# Create Mars_Data Python Dictionary
 mars_data = {}
 
+# Function for Scraping Mars Data from Web
 def scrape():
     # ------------------------------------------------- #
     # MARS NEWS
@@ -49,9 +65,7 @@ def scrape():
     # Visit URL for full-size image
     browser.visit(f'https://www.jpl.nasa.gov{featured_img}')
 
-    # ---- #
     # ADD TO DICTIONARY #
-    # ---- #
     mars_data["JPL Img"] = f'https://www.jpl.nasa.gov{featured_img}'
 
     # Close Browser
@@ -61,53 +75,61 @@ def scrape():
     # MARS FACTS
     # ------------------------------------------------- #
 
-    # # Set URL and Visit
-    # mars_facts_url = 'https://space-facts.com/mars/'
-    # mars_df = pd.read_html(mars_facts_url)[0]
+    # Set URL and Visit
+    mars_facts_url = 'https://space-facts.com/mars/'
+    mars_df = pd.read_html(mars_facts_url)[0]
 
-    # # Export as HTML and save in variable, print to make sure it looks good
-    # mars_df_html = mars_df.to_html(classes="table")
+    # Export as HTML and save in variable, print to make sure it looks good
+    mars_df_html = mars_df.to_html(classes="table")
+
+    # ADD TO DICTIONARY #
+    mars_data["Mars Facts"] = mars_df_html
 
     # # ------------------------------------------------- #
     # # MARS HEMISPHERES
     # # ------------------------------------------------- #
 
-    # # Start Splinter Again, since we closed the last browser
-    # executable_path = {'executable_path': ChromeDriverManager().install()}
-    # browser = Browser('chrome', **executable_path, headless=False)
+    # Start Splinter Again, since we closed the last browser
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-    # # Visit the USGS Astrogeology Site 
-    # usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    # browser.visit(usgs_url)
+    # Visit the USGS Astrogeology Site 
+    usgs_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(usgs_url)
 
-    # # Set HTML for Parsing, then parse with BeautifulSoup
-    # usgs_html = browser.html
-    # usgs_soup = bs(usgs_html, 'html.parser')
+    # Set HTML for Parsing, then parse with BeautifulSoup
+    usgs_html = browser.html
+    usgs_soup = bs(usgs_html, 'html.parser')
 
-    # # Loop through Links and get info, create dictionary, and append dictionary to list, go back to the first page
+    # Loop through Links and get info, create dictionary, and append dictionary to list, go back to the first page
 
-    # links = usgs_soup.select('a.product-item')
+    links = usgs_soup.select('a.product-item')
 
-    # hemisphere_image_urls = []
+    hemisphere_image_urls = []
 
-    # for link in links:
-    #     if link.text != '':
-    #         try:
-    #             browser.links.find_by_partial_text(f'{link.text}').click()
-    #             new_page_html = browser.html
-    #             new_page_soup = bs(new_page_html, 'html.parser')
-    #             img_url = new_page_soup.select('li a')[0].get('href')
-    #             img_dict = {}
-    #             img_dict["title"] = link.text
-    #             img_dict["url"] = img_url
-    #             hemisphere_image_urls.append(img_dict)
-    #             browser.visit(usgs_url)
-    #         except:
-    #             print('Link text not found.')
+    for link in links:
+        if link.text != '':
+            try:
+                browser.links.find_by_partial_text(f'{link.text}').click()
+                new_page_html = browser.html
+                new_page_soup = bs(new_page_html, 'html.parser')
+                img_url = new_page_soup.select('li a')[0].get('href')
+                img_dict = {}
+                img_dict["title"] = link.text
+                img_dict["url"] = img_url
+                hemisphere_image_urls.append(img_dict)
+                browser.visit(usgs_url)
+            except:
+                print('Link text not found.')
 
-    # browser.quit()
+    browser.quit()
 
+    # ADD TO DICTIONARY #
 
-    return print(mars_data)
+    mars_data["Mars Hemispheres"] = hemisphere_image_urls
 
-scrape()
+    # ADD TO MONGO #
+
+    collection.insert_one(mars_data)
+
+    return mars_data
